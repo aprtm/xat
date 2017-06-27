ce9973b24564ff64aabb436cf10bc22399cc6832
aprtm <apacheco@tekmexico.com>
2017-06-27 15:31:44 -0700

    folder re-structuring

--- server/index.js
+++ server/index.js
@@ -10,15 +10,15 @@ var app = express();
 var server = http.createServer(app);
 var io = socketio(server);
 app.use(express.static(path.join(__dirname, '../node_modules')));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('/', function (req, res) {
-    // console.log('serve', path.join(__dirname, 'index.html') );
-    // res.sendFile( path.join(__dirname, '../dist/index.html') );
+    console.log('serve', path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
     // res.end();
 });
-// app.get("/*", function( req, res){
-//     console.log('Request object url: ', req.baseUrl );
-// });
+app.get("/*", function (req, res) {
+    console.log('Request object url: ', req.baseUrl);
+});
 io.on('connection', function (socket) {
     console.log('A socket was opened. Client connected');
     socket.on('openSocket', function (data) { return console.log(data); });
