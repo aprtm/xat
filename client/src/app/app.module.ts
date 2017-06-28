dbc2909cde7334b472e04d7e68051347e7eb4237
aprtm <apacheco@tekmexico.com>
2017-06-28 16:35:47 -0700

    renamed component
    chat-ui --> msg-window

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ChatUiComponent } from './components/chat-ui/chat-ui.component';
+import { MsgWindowComponent } from './components/msg-window/msg-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatUiComponent
+    MsgWindowComponent
   ],
   imports: [
     BrowserModule
