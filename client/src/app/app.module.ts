2b1dd751a07563ad835fb5c28b65460015740936
aprtm <apacheco@tekmexico.com>
2017-07-02 17:27:13 -0700

    added routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,34 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MsgWindowComponent } from './components/msg-window/msg-window.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes = [
+  // {
+  //   path:'home', component:HomeComponent
+  // },
+  { path:'login', component:LoginComponent, 
+    data:{title:'Login to x@'} 
+  },
+  { path:'chat', 
+    component:MsgWindowComponent, 
+    data:{title:'x@'} 
+  },
+  // { path:'', redirectTo:'/home', pathMatch:'full'},
+  // { path:'**', redirectTo:'/home', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MsgWindowComponent
+    MsgWindowComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
