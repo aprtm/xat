ae86dd6d6cf9845ffc3ae729a76f751ac55b48ec
aprtm <apacheco@tekmexico.com>
2017-07-03 21:33:18 -0700

    added signup component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MsgWindowComponent } from './components/msg-window/msg-window.component';
@@ -8,10 +8,13 @@ import { LoginComponent } from './components/login/login.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+
 const appRoutes:Routes = [
   {
     path:'home', component:HomeComponent },
   { path:'login', component:LoginComponent },
+  { path:'signup', component:SignupComponent},
   { path:'chat', 
     component:MsgWindowComponent, 
     data:{title:'x@'} 
@@ -25,11 +28,13 @@ const appRoutes:Routes = [
     AppComponent,
     MsgWindowComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule, 
+    FormsModule,
+    ReactiveFormsModule, 
     RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
