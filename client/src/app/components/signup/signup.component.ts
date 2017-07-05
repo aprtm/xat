29408113b0662d3d6ccd20d973140234eff7f639
aprtm <apacheco@tekmexico.com>
2017-07-05 11:23:59 -0700

    created sign up component

---
+++ client/src/app/components/signup/signup.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidationErrors} from '@angular/forms';
+import { CustomValidators } from '../../validators/custom.validator'
+import { AsIterablePipe } from '../../pipes/asIterable.pipe'
+
+@Component({
+  selector: 'chat-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  formErrorMessages = {
+    'username':{
+      'required': 'Must pick a username',
+      'exists': 'That username already exists',
+      'pattern': 'Only underscore(_), letters and/or numbers are allowed'
+    },
+    'password':{
+      'required': 'Must choose a password',
+      'minlength': 'Must have at least 4 characters'
+    },
+    'passGroup':{
+      'sameText': 'Password does not match'
+    },
+    'email':{
+      'required': 'Must provide an email',
+      'email': 'Email must be similar to email@site.dom'
+    }
+  }
+
+  signUpForm: FormGroup;
+  // signUpForm = new FormGroup({
+  //   username: new FormControl(),
+  //   password: new FormControl(),
+  //   password2: new FormControl(),
+  //   email: new FormControl()
+  // });
+
+  constructor(private builder:FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.createForm();
+    // console.log(this.signUpForm.controls.passGroup);
+  }
+  
+  createForm(){
+    this.signUpForm = this.builder.group( {
+      username:['',[
+        Validators.required,
+        Validators.pattern( /^\w+$/ )
+      ]],
+
+      passGroup: this.builder.group({
+        password: ['',Validators.minLength(4)],
+        password2: ['']
+      }, { validator: CustomValidators.sameText } ),
+
+      email:['',[
+        Validators.required,
+        Validators.email
+      ]]
+
+    });
+
+    this.signUpForm.valueChanges.subscribe( ()=>{
+      //gets executed after every form change
+      // updateCtrlErrors()
+    })
+
+  }
+  
+}
