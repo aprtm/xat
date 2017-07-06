4b4a893e9d064791dc6bed56e3a961e3f4bc4e0e
aprtm <apacheco@tekmexico.com>
2017-07-06 16:48:37 -0700

    simplified validators structure
    removed the namespace/object that grouped several methods.

--- client/src/app/validators/custom.validator.ts
+++ client/src/app/validators/custom.validator.ts
@@ -1,11 +1,7 @@
 // import { FormGroup } from '@angular/forms'
 
-export var CustomValidators = {
-    sameText({value:groupValues}){
-        const [first, ...rest] = Object.keys( groupValues || {} );
-        const valid = rest.every( v => groupValues[v] === groupValues[first] );
-        return valid ? null : {sameText: true};
-    },
-
-    notANumber(){}
-};
+export function sameTextValidate({value:groupValues}){
+    const [first, ...rest] = Object.keys( groupValues || {} );
+    const valid = rest.every( v => groupValues[v] === groupValues[first] );
+return valid ? null : {sameText: true};
+}
