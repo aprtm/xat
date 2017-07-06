826344e6b32bc81d4ff52e757f0995d7126e1815
aprtm <apacheco@tekmexico.com>
2017-07-06 16:43:01 -0700

    created http service
    UsersService handles user-related requests.

---
+++ client/src/app/services/users.service.ts
@@ -0,0 +1,24 @@
+import { Http, Headers } from '@angular/http'
+import { Observable } from 'rxjs/Observable'
+import { Injectable } from '@angular/core';
+
+interface User{
+  username:string
+  password:string
+  email:string
+}
+
+@Injectable()
+export class UsersService {
+  private header:Headers = new Headers( {'Content-Type':'application/json'} )
+  constructor(private http:Http) { }
+
+  addUser(user:User){
+    
+    this.http.post( '/api/users', user, {headers:this.header} )
+      .subscribe( ( resp )=>console.log('resp: ',resp), ( err )=>console.log('err: ',err) ) ;
+    
+    console.log('Posted to server: ', user);
+  }
+
+}
