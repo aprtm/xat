861558bda139bbb170af31117872f921d15931e2
aprtm <apacheco@tekmexico.com>
2017-07-21 11:01:17 -0700

    Improved lists ui
    Properly displays the chats and friends lists of the logged user.

--- client/src/app/components/manager/manager.component.ts
+++ client/src/app/components/manager/manager.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SessionService } from '../../services/session.service';
+import { UsersService } from '../../services/users.service'
+
+import { User, Contact } from '../../interfaces/Users';
+import { Conversation } from '../../interfaces/Conversations';
+
+
 @Component({
   selector: 'chat-manager',
   templateUrl: './manager.component.html',
@@ -7,7 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerComponent implements OnInit {
 
-  constructor() { }
+  // private currentFriends:User[] = [];
+  // private currentUser:User = null;
+
+  private currentList:User[]|Conversation[] = []
+  private otherList:User[]|Conversation[] = [];
+  private visibleList:string;
+  private hiddenList:string;
+
+  constructor( private sessionService:SessionService, private usersService:UsersService ) {
+    
+    if( sessionService.isActive() ){
+      this.visibleList = 'Friends';
+      this.hiddenList = 'Chats';
+      this.currentList = this.sessionService.getSession().user.friends;
+      this.otherList = this.sessionService.getSession().user.conversations;
+    }
+
+  }
+
+  switchList(){
+      let swapName = this.visibleList;
+      this.visibleList = this.hiddenList;
+      this.hiddenList = swapName;
+
+      let swapList = this.currentList;
+      this.currentList = this.otherList;
+      this.otherList = swapList;
+  }
 
   ngOnInit() {
   }
